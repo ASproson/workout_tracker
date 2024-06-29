f7c5f66c90394078934fe38c1c6aaabe68ae6a6a
ASproson <a.sproson@outlook.com>
2024-06-29 18:12:15 +0100

    Moves components to render pages

--- src/pages/CurrentWorkout.tsx
+++ src/pages/CurrentWorkout.tsx
@@ -1,7 +1,10 @@
+import { WorkoutContainer } from "../components/WorkoutContainer";
+
 export const CurrentWorkout = () => {
   return (
     <div>
       <h1>Current Workout</h1>
+      <WorkoutContainer />
     </div>
   );
 };
