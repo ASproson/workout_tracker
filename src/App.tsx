41c48320270b663fd3f7469537e1321ea1985551
ASproson <a.sproson@outlook.com>
2024-06-29 10:03:52 +0100

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,75 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  return (
+    <div className="h-screen ">
+      <h1 className=" text-center">Testing workflow push</h1>
+      <WorkoutContainer />
+    </div>
+  );
+}
+
+export default App;
+
+/**
+ * Main controller for centering and responsive design
+ * @returns
+ */
+const WorkoutContainer = () => {
+  const [showWorkout, setShowWorkout] = useState(false);
+  return (
+    <div>
+      <h1>Week #1</h1>
+      <div className="flex">
+        <h2 className="mr-2">Workout #1</h2>
+        <button onClick={() => setShowWorkout(!showWorkout)}>Show</button>
+      </div>
+      {showWorkout && <WorkoutTable />}
+    </div>
+  );
+};
+
+/**
+ * Show/hide toggle for this component, when title is clicked, exercise is shown
+ * @returns
+ */
+const WorkoutTable = () => {
+  const tableHeaders = [
+    { id: 0, name: "Exercise" },
+    { id: 2, name: "Sets" },
+    { id: 1, name: "Reps" },
+  ];
+
+  const exercies = [
+    { id: 0, exercise: "Bench Press", sets: 4, reps: 4 },
+    { id: 1, exercise: "Deadlift", sets: 4, reps: 4 },
+  ];
 
   return (
     <div>
-      <h1 className='text-red-500 text-center'>Testing workflow push</h1>
+      <table className="border-2 border-black text-center">
+        <thead className="border-2 border-black">
+          <tr>
+            {tableHeaders.map((h) => (
+              <th key={h.id} scope="col" className="border-2 border-black">
+                {h.name}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {exercies.map((e) => (
+            <tr key={e.id} className="border-2 border-black">
+              <th scope="row" className="border-2 border-black">
+                {e.exercise}
+              </th>
+              <td className="border-2 border-black">{e.reps}</td>
+              <td className="border-2 border-black">{e.sets}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
-
-export default App
+  );
+};
