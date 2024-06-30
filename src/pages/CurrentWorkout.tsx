b4993fb061e5caef16104d6126cbb35030df655a
ASproson <a.sproson@outlook.com>
2024-06-30 09:54:07 +0100

    Removes placeholder title

--- src/pages/CurrentWorkout.tsx
+++ src/pages/CurrentWorkout.tsx
@@ -3,7 +3,6 @@ import { WorkoutContainer } from "../components/WorkoutContainer";
 export const CurrentWorkout = () => {
   return (
     <div>
-      <h1>Current Workout</h1>
       <WorkoutContainer />
     </div>
   );
