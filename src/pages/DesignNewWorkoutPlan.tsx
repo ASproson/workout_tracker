89578315bf0cbbdc987d7fd945f6c8364ed6f937
ASproson <a.sproson@outlook.com>
2024-07-01 08:24:57 +0100

    Fixes key issue

--- src/pages/DesignNewWorkoutPlan.tsx
+++ src/pages/DesignNewWorkoutPlan.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export const DesignNewWorkoutPlan = () => {
   return (
@@ -7,20 +7,20 @@ export const DesignNewWorkoutPlan = () => {
         <WorkoutPlanBuilder />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const WorkoutPlanBuilder = () => {
-  const [premade, setPremade] = useState(false)
-  const [custom, setCustom] = useState(false)
+  const [premade, setPremade] = useState(false);
+  const [custom, setCustom] = useState(false);
 
   return (
     <div className="text-center">
       <div className="mb-4">
         <button
           onClick={() => {
-            setPremade(!premade)
-            setCustom(false)
+            setPremade(!premade);
+            setCustom(false);
           }}
         >
           Premade Workout Plans
@@ -29,8 +29,8 @@ export const WorkoutPlanBuilder = () => {
       <div className="mb-4">
         <button
           onClick={() => {
-            setCustom(!custom)
-            setPremade(false)
+            setCustom(!custom);
+            setPremade(false);
           }}
         >
           Create New Workout Plan
@@ -39,90 +39,90 @@ export const WorkoutPlanBuilder = () => {
       {premade && <PremadeWorkoutPlans />}
       {custom && <CustomWorkoutPlans />}
     </div>
-  )
-}
+  );
+};
 
 export const PremadeWorkoutPlans = () => {
-  const [showPlan, setShowPlan] = useState<Set<string>>(new Set())
+  const [showPlan, setShowPlan] = useState<Set<string>>(new Set());
 
   const plans = [
     {
-      name: '2 Day Plans',
+      name: "2 Day Plans",
       splits: [
-        'Full Body', // Add the list of exercises across days here? >> Preview/Select, worth doing a modal?
-        'Upper/Lower',
-        'Push/Pull',
-        'Arm Builder',
-        'Chest Builder',
-        'Leg Builder',
-        'Back Builder',
+        "Full Body", // Add the list of exercises across days here? >> Preview/Select, worth doing a modal?
+        "Upper/Lower",
+        "Push/Pull",
+        "Arm Builder",
+        "Chest Builder",
+        "Leg Builder",
+        "Back Builder",
       ],
     },
     {
-      name: '3 Day Plans',
+      name: "3 Day Plans",
       splits: [
-        'Full Body',
-        'Upper/Lower',
-        'Push/Pull/Legs',
-        'Arm Builder',
-        'Chest Builder',
-        'Leg Builder',
-        'Back Builder',
+        "Full Body",
+        "Upper/Lower",
+        "Push/Pull/Legs",
+        "Arm Builder",
+        "Chest Builder",
+        "Leg Builder",
+        "Back Builder",
       ],
     },
     {
-      name: '4 Day Plans',
+      name: "4 Day Plans",
       splits: [
-        'Full Body',
-        'Upper/Lower',
-        'Push/Pull',
-        'Push/Pull/Legs/Full Body',
-        'Arm Builder',
-        'Chest Builder',
-        'Leg Builder',
-        'Back Builder',
+        "Full Body",
+        "Upper/Lower",
+        "Push/Pull",
+        "Push/Pull/Legs/Full Body",
+        "Arm Builder",
+        "Chest Builder",
+        "Leg Builder",
+        "Back Builder",
       ],
     },
     {
-      name: '5 Day Plans',
+      name: "5 Day Plans",
       splits: [
-        'Full Body',
-        'Upper/Lower',
-        'Push/Pull',
-        'Push/Pull/Legs/Upper/Lower',
-        'Arm Builder',
-        'Chest Builder',
-        'Leg Builder',
-        'Back Builder',
+        "Full Body",
+        "Upper/Lower",
+        "Push/Pull",
+        "Push/Pull/Legs/Upper/Lower",
+        "Arm Builder",
+        "Chest Builder",
+        "Leg Builder",
+        "Back Builder",
       ],
     },
-  ]
+  ];
 
   const handleShowPlan = (day: string) => {
     setShowPlan((prevState) => {
-      const newSet = new Set(prevState)
+      const newSet = new Set(prevState);
       if (newSet.has(day)) {
-        newSet.delete(day)
+        newSet.delete(day);
       } else {
-        newSet.add(day)
+        newSet.add(day);
       }
-      return newSet
-    })
-  }
+      return newSet;
+    });
+  };
 
   return (
     <div>
       <h1>Premade</h1>
       <ul className="text-left">
         {plans.map((p) => (
-          <li>
+          <li key={p.name}>
             <button key={p.name} onClick={() => handleShowPlan(p.name)}>
               {p.name}
             </button>
             {showPlan.has(p.name) && (
               <ul className="ml-4">
                 {p.splits.map((s) => (
-                  <li>{s}</li>
+                  <li key={s}>{s}</li>
                 ))}
               </ul>
             )}
@@ -130,13 +130,13 @@ export const PremadeWorkoutPlans = () => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export const CustomWorkoutPlans = () => {
   return (
     <div>
       <h1>Custom</h1>
     </div>
-  )
-}
+  );
+};
