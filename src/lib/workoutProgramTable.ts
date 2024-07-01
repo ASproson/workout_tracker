fe9d2fdf2e3346bb0b5b5493ba387b1d9b616050
ASproson <a.sproson@outlook.com>
2024-07-01 13:22:35 +0100

    Adds initial two day plans for testing

--- src/lib/workoutProgramTable.ts
+++ src/lib/workoutProgramTable.ts
@@ -1,25 +1,51 @@
 export const twoDayWorkoutPrograms = [
   {
-    program_name: "Two Day Full Body",
+    program_name: "Full Body",
     program_id: "adasd123",
-    duration: 12,
-    description:
-      "This full body workout places compound exercises early on, and finishes with isolation exercises for optimal growth with as little possible time in the gym. For those even shorter on time, you can superset every two exercises. Plan to have a few rest days inbetween workouts, i.e: Monday/Thursday.",
-    day_one_exercises: [
-      "Bench Press",
-      "Squat",
-      "Bent-over Row",
-      "Lateral Raise",
-      "Cable Bicep Curl",
-      "Tricep Pushdown",
-    ],
-    day_two_exercises: [
-      "Incline Bench Press",
-      "Romanian Deadlift",
-      "Leg Press",
-      "Upright Row",
-      "Bicep Curl",
-      "Cable Tricep Extension",
-    ],
+    program: {
+      duration: 12,
+      description:
+        "This full body plan places compound exercises early on, and finishes with isolation exercises for optimal growth with as little possible time in the gym. For those even shorter on time, you can superset every two exercises. Plan to have a few rest days inbetween workouts, i.e: Monday/Thursday.",
+      day_one_exercises: [
+        "Bench Press",
+        "Squat",
+        "Bent-over Row",
+        "Lateral Raise",
+        "Cable Bicep Curl",
+        "Tricep Pushdown",
+      ],
+      day_two_exercises: [
+        "Incline Bench Press",
+        "Romanian Deadlift",
+        "Leg Press",
+        "Upright Row",
+        "Bicep Curl",
+        "Cable Tricep Extension",
+      ],
+    },
+  },
+  {
+    program_name: "Upper Lower",
+    program_id: "vawedaw123",
+    program: {
+      duration: 12,
+      description:
+        "This plan splits the week into an upper body workout, and a lower body workout. For those that want to maximize muscle growth with minimal time in the gym. This one goes hard. Plan to have a few rest days inbetween workouts, i.e: Monday/Thursday",
+      day_one_exercises: [
+        "Bench Press",
+        "Overhead Press",
+        "Lat Pulldown",
+        "Cable Flye",
+        "Cable Bicep Curl",
+        "Cable Tricep Pushdown",
+      ],
+      day_two_exercises: [
+        "Squat",
+        "Romanian Deadlift",
+        "Leg Extension",
+        "Lying Leg Curl",
+        "Calf raise",
+      ],
+    },
   },
 ];
