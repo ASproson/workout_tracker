99641982c19ce35cf620b38cf5cfdea6ec92344f
ASproson <a.sproson@outlook.com>
2024-07-01 14:07:39 +0100

    Sets up rendering for premade plans

--- src/pages/DesignNewWorkoutPlan.tsx
+++ src/pages/DesignNewWorkoutPlan.tsx
@@ -1,4 +1,10 @@
 import { useState } from "react";
+import { workoutProgramsTable } from "../lib/workoutProgramTable";
+import {
+  WorkoutCategoryProps,
+  WorkoutPlansProps,
+  ExerciseListProps,
+} from "../lib/types";
 
 export const DesignNewWorkoutPlan = () => {
   return (
@@ -42,96 +48,75 @@ export const WorkoutPlanBuilder = () => {
   );
 };
 
-export const PremadeWorkoutPlans = () => {
-  const [showPlan, setShowPlan] = useState<Set<string>>(new Set());
+export const PremadeWorkoutPlans = () => (
+  <div>
+    <h1>Premade Workout Plans</h1>
+    <ul className="text-left">
+      {workoutProgramsTable.map((categoryData, index) => (
+        <WorkoutCategory
+          key={index}
+          category={categoryData.category}
+          plans={categoryData.plans}
+        />
+      ))}
+    </ul>
+  </div>
+);
 
-  const plans = [
-    {
-      name: "2 Day Plans",
-      splits: [
-        "Full Body", // Add the list of exercises across days here? >> Preview/Select, worth doing a modal?
-        "Upper/Lower",
-        "Push/Pull",
-        "Arm Builder",
-        "Chest Builder",
-        "Leg Builder",
-        "Back Builder",
-      ],
-    },
-    {
-      name: "3 Day Plans",
-      splits: [
-        "Full Body",
-        "Upper/Lower",
-        "Push/Pull/Legs",
-        "Arm Builder",
-        "Chest Builder",
-        "Leg Builder",
-        "Back Builder",
-      ],
-    },
-    {
-      name: "4 Day Plans",
-      splits: [
-        "Full Body",
-        "Upper/Lower",
-        "Push/Pull",
-        "Push/Pull/Legs/Full Body",
-        "Arm Builder",
-        "Chest Builder",
-        "Leg Builder",
-        "Back Builder",
-      ],
-    },
-    {
-      name: "5 Day Plans",
-      splits: [
-        "Full Body",
-        "Upper/Lower",
-        "Push/Pull",
-        "Push/Pull/Legs/Upper/Lower",
-        "Arm Builder",
-        "Chest Builder",
-        "Leg Builder",
-        "Back Builder",
-      ],
-    },
-  ];
+const WorkoutCategory = ({ category, plans }: WorkoutCategoryProps) => {
+  const [showPlan, setShowPlan] = useState(false);
 
-  const handleShowPlan = (day: string) => {
-    setShowPlan((prevState) => {
-      const newSet = new Set(prevState);
-      if (newSet.has(day)) {
-        newSet.delete(day);
-      } else {
-        newSet.add(day);
-      }
-      return newSet;
-    });
-  };
+  const handleToggle = () => setShowPlan(!showPlan);
 
   return (
-    <div>
-      <h1>Premade</h1>
-      <ul className="text-left">
-        {plans.map((p) => (
-          <li key={p.name}>
-            <button key={p.name} onClick={() => handleShowPlan(p.name)}>
-              {p.name}
-            </button>
-            {showPlan.has(p.name) && (
-              <ul className="ml-4">
-                {p.splits.map((s) => (
-                  <li key={s}>{s}</li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <li>
+      <button onClick={handleToggle}>{category}</button>
+      {showPlan && (
+        <ul className="ml-4">
+          {plans.map((plan) => (
+            <WorkoutPlan key={plan.program_id} plan={plan} />
+          ))}
+        </ul>
+      )}
+    </li>
   );
 };
+
+const WorkoutPlan = ({ plan }: WorkoutPlansProps) => {
+  const [showPlan, setShowPlan] = useState(false);
+  return (
+    <li key={plan.program_id} onClick={() => setShowPlan(!showPlan)}>
+      <button>{plan.program_name}</button>
+      {showPlan && (
+        <>
+          <ExerciseList
+            title="Day One Exercises:"
+            description={plan.program.description}
+            exercises={plan.program.day_one_exercises}
+          />
+          <ExerciseList
+            title="Day Two Exercises:"
+            exercises={plan.program.day_two_exercises}
+          />
+        </>
+      )}
+    </li>
+  );
+};
+
+const ExerciseList = ({ title, exercises, description }: ExerciseListProps) => (
+  <div className="my-2">
+    <p className="mb-2">{description}</p>
+    <h4>{title}</h4>
+    <ul>
+      {exercises.map((exercise, idx) => (
+        <li key={idx} className="ml-4">
+          {exercise}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export const CustomWorkoutPlans = () => {
   return (
